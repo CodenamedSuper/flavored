e449ac0bc6be0491c76098835f1e54cf7727cf64
Super1166 <110970817+Super1166@users.noreply.github.com>
2025-02-01 23:35:23 +0200

    cleanup

--- src/main/java/com/codenamed/flavored/registry/FlavoredBlockTags.java
+++ src/main/java/com/codenamed/flavored/registry/FlavoredBlockTags.java
@@ -7,7 +7,6 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class FlavoredBlockTags {
-    public static final TagKey<Block> SALT_POOL_REPLACEABLE = create("salt_pool_replaceable");
 
 
     private static TagKey<Block> create(String name) {
